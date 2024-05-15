7ce3fb594eca24136670ebfe6cce6750cffe0386
Andres Morales Martinez <18030666@itcelaya.edu.mx>
2024-05-15 14:01:08 -0600

    Done

--- src/examen/pkg3/Examen3.java
+++ src/examen/pkg3/Examen3.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Examen3{
     private enum Estado {
@@ -16,7 +17,7 @@ public class Examen3{
     }
     
     private enum Valido{
-        CORRECTO, INCORRECTO
+        CORRECTA, INCORRECTA
     }
     
     private Estado estadoActual;
@@ -26,162 +27,162 @@ public class Examen3{
         this.estadoActual = Estado.q0;
     }
     
-    public Valido analizar (String entrada){
-        for (char caracter : entrada.toCharArray()) {
-            transicion(caracter);
+    public Valido analizar (String[] entrada){
+        for (String elemento : entrada) {
+            transicion(elemento);
         }
         switch (estadoActual) {
             case q0:
-                return Valido.INCORRECTO;
+                return Valido.INCORRECTA;
             case q1:
-                return Valido.INCORRECTO;
+                return Valido.INCORRECTA;
             case q2:
-                return Valido.INCORRECTO;
+                return Valido.INCORRECTA;
             case q3:
-                return Valido.INCORRECTO;
+                return Valido.INCORRECTA;
             case q4:
-                return Valido.INCORRECTO;
+                return Valido.INCORRECTA;
             case q5:
-                return Valido.INCORRECTO;
+                return Valido.INCORRECTA;
             case q6:
-                return Valido.INCORRECTO;
+                return Valido.INCORRECTA;
             case q7:
-                return Valido.INCORRECTO;
+                return Valido.INCORRECTA;
             case q8:
-                return Valido.INCORRECTO;
+                return Valido.INCORRECTA;
             case q9:
-                return Valido.INCORRECTO;
+                return Valido.INCORRECTA;
             case q10:
-                return Valido.INCORRECTO;
+                return Valido.INCORRECTA;
             case q11:
-                return Valido.INCORRECTO;
+                return Valido.INCORRECTA;
             case q12:
-                return Valido.INCORRECTO;
+                return Valido.INCORRECTA;
             case q13:
-                return Valido.INCORRECTO;
+                return Valido.INCORRECTA;
             case q14:
-                return Valido.CORRECTO;
+                return Valido.CORRECTA;
             default:
                 return null;
         }
     }
     
-    private void transicion(char caracter){
+    private void transicion(String elemento){
         switch (estadoActual) {
             case q0:
-                if (caracter == 'd') {
+                if (elemento.matches("^[A-Z][a-zA-Z]+$")) {
                     estadoActual = Estado.q1;
                     CadenaSalida = CadenaSalida + "q1 ";
                     
-                } else if (caracter == 'e'){
+                } else if (elemento.matches("^[A-Z]+\\.$")){
                     estadoActual = Estado.q2;
                     CadenaSalida = CadenaSalida + "q2 ";
                 }
                 break;
             case q1:
-                if (caracter == 'd') {
+                if (elemento.matches("^[A-Z][a-zA-Z]+$")) {
                     estadoActual = Estado.q1;
                     CadenaSalida = CadenaSalida + "q1 ";
                     
-                } else if (caracter == 'a'){
+                } else if (elemento.matches("^[A-Z][a-zA-Z]+\\.$")){
                     estadoActual = Estado.q3;
                     CadenaSalida = CadenaSalida + "q3 ";
-                } else if (caracter == 'e'){
+                } else if (elemento.matches("^[A-Z]+\\.$")){
                     estadoActual = Estado.q2;
                     CadenaSalida = CadenaSalida + "q2 ";
                 }
                 break;
             case q2:
-                if (caracter == 'e') {
+                if (elemento.matches("^[A-Z]+\\.$")) {
                     estadoActual = Estado.q2;
                     CadenaSalida = CadenaSalida + "q2 ";
                     
-                } else if (caracter == 'd'){
+                } else if (elemento.matches("^[A-Z][a-zA-Z]+$")){
                     estadoActual = Estado.q1;
                     CadenaSalida = CadenaSalida + "q1 ";
-                } else if (caracter == 'a'){
+                } else if (elemento.matches("^[A-Z][a-zA-Z]+\\.$")){
                     estadoActual = Estado.q3;
                     CadenaSalida = CadenaSalida + "q3 ";
                 }
                 break;
             case q3:
-                if (caracter == 'b') {
+                if (elemento.matches("^[A-Z][a-zA-Z]+\\.$")) {
                     estadoActual = Estado.q4;
                     CadenaSalida = CadenaSalida + "q4 ";
                     
-                } else if (caracter == 'c'){
+                } else if (elemento.equals("<SaltoDeLinea>")){
                     estadoActual = Estado.q5;
                     CadenaSalida = CadenaSalida + "q5 ";
                 }
                 break;
             case q4:
-                if (caracter == 'c') {
+                if (elemento.equals("<SaltoDeLinea>")) {
                     estadoActual = Estado.q5;
                     CadenaSalida = CadenaSalida + "q5 ";
                     
                 }
                 break;
             case q5:
-                if (caracter == 'f') {
+                if (elemento.matches("^[A-Z][a-zA-Z]+$")) {
                     estadoActual = Estado.q6;
                     CadenaSalida = CadenaSalida + "q6 ";
                     
-                } else if (caracter == 'g'){
+                } else if (elemento.matches("^#[0-9]+$")){
                     estadoActual = Estado.q7;
                     CadenaSalida = CadenaSalida + "q7 ";
                 }
                 break;
             case q6:
-                if (caracter == 'g') {
+                if (elemento.matches("^#[0-9]+$")) {
                     estadoActual = Estado.q7;
                     CadenaSalida = CadenaSalida + "q7 ";
                     
                 }
                 break;
             case q7:
-                if (caracter == 'h') {
+                if (elemento.matches("^[A-Z][a-zA-Z]+$")) {
                     estadoActual = Estado.q8;
                     CadenaSalida = CadenaSalida + "q8 ";
                     
                 }
                 break;
             case q8:
-                if (caracter == 'c') {
+                if (elemento.equals("<SaltoDeLinea>")) {
                     estadoActual = Estado.q9;
                     CadenaSalida = CadenaSalida + "q9 ";
                     
                 } 
                 break;
             case q9:
-                if (caracter == 'i') {
-                    estadoActual = Estado.q10;
-                    CadenaSalida = CadenaSalida + "q10 ";
-                    
-                } 
-                break;
-            case q10:
-                if (caracter == ',') {
+                if (elemento.matches("^[A-Z][a-zA-Z]+,$")) {
                     estadoActual = Estado.q11;
                     CadenaSalida = CadenaSalida + "q11 ";
                     
                 } 
                 break;
+//            case q10:
+//                if (elemento.matches("")) {
+//                    estadoActual = Estado.q11;
+//                    CadenaSalida = CadenaSalida + "q11 ";
+//                    
+//                } 
+//                break;
             case q11:
-                if (caracter == 'k') {
+                if (elemento.matches("^[A-Z]{2}$")) {
                     estadoActual = Estado.q12;
                     CadenaSalida = CadenaSalida + "q12 ";
                     
                 } 
                 break;
             case q12:
-                if (caracter == 'j') {
+                if (elemento.matches("^[0-9]{5}$")) {
                     estadoActual = Estado.q13;
                     CadenaSalida = CadenaSalida + "q13 ";
                     
                 } 
                 break;
             case q13:
-                if (caracter == 'c') {
+                if (elemento.equals("<SaltoDeLinea>")) {
                     estadoActual = Estado.q14;
                     CadenaSalida = CadenaSalida + "q14 ";
                     
@@ -192,6 +193,7 @@ public class Examen3{
 
     public static void main(String[] args) {
         Examen3 cadena = new Examen3();
+        String[] cadena3 = {};
 //        Scanner leer=new Scanner(System.in);
 //        String entrada;
 //        entrada=leer.nextLine();
@@ -231,21 +233,24 @@ public class Examen3{
             String contenidoString = contenido.toString();
 
             // Mostrar el contenido del archivo
-            System.out.println("Contenido del archivo:");
-            System.out.println(contenidoString);
+            //System.out.println("Contenido del archivo:");
+            //System.out.println(contenidoString);
 
             // Separar la cadena en un arreglo de strings
             String[] lines = contenidoString.split("\n");
-            String[] elementos = contenidoString.split("\\s+|\n");
-            System.out.println("lineas separadas:");
+            String cadena2="";
+            //System.out.println("lineas separadas:");
             for (String line : lines) {
-                System.out.println(line);
+                //System.out.println(line);
+                cadena2 = cadena2 + line + " <SaltoDeLinea> ";
             }
-            System.out.println("");
-
+            //System.out.println("");
+            //System.out.println(cadena2);
+            //System.out.println("");
+            cadena3 = cadena2.split("\\s");
             // Mostrar los elementos
             System.out.println("Elementos separados:");
-            for (String elemento : elementos) {
+            for (String elemento : cadena3) {
                 System.out.println(elemento);
             }
         } catch (IOException e) {
@@ -254,8 +259,8 @@ public class Examen3{
         }
         
         
-//        Valido valido = cadena.analizar(entrada);
-//        System.out.println(CadenaSalida);  
-//        System.out.println(valido);
+        Valido valido = cadena.analizar(cadena3);
+        System.out.println(CadenaSalida);  
+        System.out.println("La direccion es: "+valido);
     }
 }
